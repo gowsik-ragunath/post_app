2e64e29d5356b782f576f5273187fcb7d7eedfb4
gowsikragunath <v.gowsik@gmail.com>
2019-01-10 15:24:16 +0530

    post features added completely

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,31 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(post_paramas)
 		@post.save
-		redirect_to(root_path)
+		redirect_to(posts_path)
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to(posts_path)
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		if @post.update_attributes(post_paramas)
+			redirect_to(posts_path)
+		else
+			@post = Post.find(params[:id])
+			render action: :edit
+		end	
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'posts#index'
-  get '/posts/new', to: 'posts#new'
-  post '/posts', to: 'posts#create'
-  # resources :posts
+  # root to: 'posts#index'
+  # get '/posts/new', to: 'posts#new'
+  # post '/posts', to: 'posts#create'
+  # get '/posts/:id', to: 'posts#show' ,as: 'post'
+  # delete '/posts/:id', to: 'posts#delete'
+  # post 'posts/:id', to: 'posts#edit'
+  resources :posts
 end
 	
