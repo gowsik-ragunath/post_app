10bc3b159494c50bd6c8cd794edc9821cdd56420
gowsikragunath <v.gowsik@gmail.com>
2019-02-01 17:50:39 +0530

    Re-Adding git ignored file

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  has_and_belongs_to_many :posts, join_table: :posts_users_reads
+  has_many :comments, dependent: :destroy
+
+end
